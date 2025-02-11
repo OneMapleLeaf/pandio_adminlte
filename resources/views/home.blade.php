c46ce8cdfab27ced7a7139c58ce9c810fb5eba9b
James Pandio <114295563+OneMapleLeaf@users.noreply.github.com>
2025-02-11 12:37:27 +0800

    added profile info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,92 @@
 @extends('layouts.app')
 
 @section('content')
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">{{ __('Dashboard') }}</h1>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
+<!-- Main content -->
+<!-- Content Header (Page header) -->
+<section class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1>Profile</h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item active">User Profile</li>
+        </ol>
+      </div>
     </div>
-    <!-- /.content-header -->
+  </div><!-- /.container-fluid -->
+</section>
+<!-- /.content -->
 
-    <!-- Main content -->
-    <div class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <p class="card-text">
-                                {{ __('You are logged in!') }}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- /.row -->
-        </div><!-- /.container-fluid -->
+<!-- Profile Image -->
+<div class="card card-primary card-outline">
+  <div class="card-body box-profile">
+    <div class="text-center">
+      <img class="profile-user-img img-fluid img-circle"
+        src="{{ URL::to('/') }}/images/apn.png"
+        alt="User profile picture">
     </div>
-    <!-- /.content -->
+
+    <h3 class="profile-username text-center">James Benedict Pandio</h3>
+
+    <p class="text-muted text-center">Software Engineer</p>
+
+    <ul class="list-group list-group-unbordered mb-3">
+      <li class="list-group-item">
+        <b>Followers</b> <a class="float-right">911</a>
+      </li>
+      <li class="list-group-item">
+        <b>Following</b> <a class="float-right">420</a>
+      </li>
+      <li class="list-group-item">
+        <b>Friends</b> <a class="float-right">6,969</a>
+      </li>
+    </ul>
+
+    <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+
+    <div class="card card-primary m-4">
+      <div class="card-header">
+        <h3 class="card-title">About Me</h3>
+      </div>
+      <!-- /.card-header -->
+      <div class="card-body">
+        <strong><i class="fas fa-book mr-1"></i> Education</strong>
+
+        <p class="text-muted">
+          B.S. in Information Technology from Don Honorio Ventura State University 
+        </p>
+
+        <hr>
+
+        <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
+
+        <p class="text-muted">Apalit, Pampanga</p>
+
+        <hr>
+
+        <strong><i class="fas fa-pencil-alt mr-1"></i> Skills</strong>
+
+        <p class="text-muted">
+          <span class="tag tag-danger">UI Design</span>
+          <span class="tag tag-success">Coding</span>
+          <span class="tag tag-info">Javascript</span>
+          <span class="tag tag-warning">PHP</span>
+          <span class="tag tag-primary">Node.js</span>
+        </p>
+
+        <hr>
+
+        <strong><i class="far fa-file-alt mr-1"></i> Notes</strong>
+
+        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
+      </div>
+      <!-- /.card-body -->
+    </div>
+
+  </div>
+  <!-- /.card-body -->
+</div>
 @endsection
